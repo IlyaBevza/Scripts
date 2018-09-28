ab8bc81c3a5f8189d1c6926f74ec68fdd2afe547
INBevza <ilya_bevza@hotmail.com>
2018-09-28 16:36:11 +0300

    Остановка, запуск, пеерзапуск служб 1С

---
+++ Services_1C.ps1
@@ -0,0 +1,38 @@
+Write-Host "Выберите сервер 1С (указав номер) из списка или укажите другой"
+Write-Host "1. Боевой сервер"
+Write-Host "2. Сервер разработчиков"
+Write-Host "3. Выйти из скрипта"
+
+$choice=Read-Host "Ваш выбор: "
+$server = $choice
+switch ($choice) {
+    1 { $server="srv-mssql" }
+    2 {$server = "srv-1c-dev"}
+    3 {exit}
+    Default {$server=$choice}
+}
+
+$service = Get-Service -ComputerName $server -Name *1C*
+
+$service
+
+Write-Host "Что нужно сделать со службой?"
+Write-Host "1. Перестартовать её"
+Write-Host "2. Остановить ёё"
+Write-Host "3. Запустить её"
+Write-Host "Любой другой символ - Выйти из скрипта"
+
+$choice=Read-Host "Ваш выбор: "
+switch ($choice) {
+    1 { Restart-Service $service }
+    2 {
+        if ($service.Status -ne "Stopped"){
+            Stop-Service $service
+        }
+    }
+    3 {
+        if ($service.Status -ne "Running"){
+            Start-Service $service}
+    }
+    Default {exit}
+}
