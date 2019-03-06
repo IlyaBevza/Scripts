ff01ce13286194b598cba695efbd778e05d45274
Бевза Илья Николаевич <i.bevza@fguppromservis.ru>
2019-03-06 11:55:23 +0300

    ..

--- ErrorsLog.ps1
+++ ErrorsLog.ps1
@@ -2,14 +2,14 @@
 $after= $now.AddDays(-3)
 $after=$after.Date
 Write-Host "Вывод данных об ошибках за 3 дня"
-Write-Host "======================================="
+Write-Host "============================================="
 Write-Host "============= Текущий компьютер ============="
 Get-EventLog -LogName Application -EntryType Error -Before $now -After $after
 #Get-EventLog -LogName System -EntryType Error -Before $now -After $after
 $servers="srv-mssql01","srv-mssql","srv-ps1c-02","srv-1c-dev"
 foreach ($server in $servers)
 {
-    Write-Host "======================================="
+    Write-Host "=============================================="
     Write-Host "============= Сервер " $server " ============="
     Get-EventLog -ComputerName $server  -LogName Application -EntryType Error -Before $now -After $after
     #Get-EventLog -ComputerName $server  -LogName System -EntryType Error -Before $now -After $after
---
+++ СамыеТяжелыеПроцессы.ps1
@@ -0,0 +1,6 @@
+﻿$server= Read-Host "Укажите имя компьютера. По умолчанию - это текущий комп"
+if($server -eq '')
+{
+    $server= $env:COMPUTERNAME
+}
+gps -ComputerName $server | sort WS -Descending | select -First 5
